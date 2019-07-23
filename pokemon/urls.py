c0959e8d26603f6a203a1edad9989d05e537802a
gorvernment <48075099+gorvernment@users.noreply.github.com>
2019-07-23 17:33:13 +0900

    pokemon list를 JavaScript를 통해 로딩토록 처리

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.sitemaps.views import index
 from django.urls import path
 
-from pokemon.views import index, pokemon_edit, pokemon_new
+from pokemon.views import index, pokemon_edit, pokemon_new, pokemon_list_html
 
 urlpatterns = [
     path('', index),
+    path('pokemon_list.html', pokemon_list_html),
     path('new/', pokemon_new),
     path('<int:pk>/', pokemon_edit),
     # re_path(r'(?P<pk>\d+)', pokemon_edit())
