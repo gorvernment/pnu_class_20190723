3aa850ed3a6972f7ce37856fd72602e67ae8c8b7
gorvernment <48075099+gorvernment@users.noreply.github.com>
2019-07-24 10:43:04 +0900

    pokemon_list.html에 veamcamp 실습 스타일 적용

--- pokemon/views.py
+++ pokemon/views.py
@@ -5,15 +5,18 @@ from pokemon.models import Pokemon
 
 
 def index(request):
-    # return render(request, 'root.html')
-    return render(request, 'pokemon/pokemon_list.html')
-
-def pokemon_list_html(request):
     qs = Pokemon.objects.all()  # QuerySet 타입
     # return render(request, 'root.html')
-    return render(request, 'pokemon/pokemon_list_html.html', {
+    return render(request, 'pokemon/pokemon_list.html', {
         'pokemon_list': qs,
     })
+
+# def pokemon_list_html(request):
+#     qs = Pokemon.objects.all()  # QuerySet 타입
+#     # return render(request, 'root.html')
+#     return render(request, 'pokemon/pokemon_list.html', {
+#         'pokemon_list': qs,
+#     })
 
 def pokemon_new(request):
     return HttpResponse("새 포켓몬 등록 Form")
