f11f1479c72de1e04ee5d51c83a5671bd1ab139b
gorvernment <48075099+gorvernment@users.noreply.github.com>
2019-07-24 13:50:10 +0900

    pokemon_edit 뷰 구현

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', index),
     # path('pokemon_list.html', pokemon_list_html),
     path('new/', pokemon_new),
-    path('<int:pk>/', pokemon_edit),
+    path('<int:pk>/edit/', pokemon_edit),
     # re_path(r'(?P<pk>\d+)', pokemon_edit())
 
 ]
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from pokemon.forms import PokemonForm
 from pokemon.models import Pokemon
@@ -26,7 +26,7 @@ def pokemon_new(request):
     else:
         # 뷰에서 참조할 수 있는 데이터 목록
         # request.GET, request.POST, request.FILES
-        form = PokemonForm(request.POST, request.FILES)
+        form = PokemonForm(request.POST, request.FILES) # insert : instance가 없기 때문
         if form.is_valid():
             form.save()  # ModelForm에서만 지원
             return redirect('/pokemon/')
@@ -38,6 +38,23 @@ def pokemon_new(request):
     })
 
 def pokemon_edit(request, pk):
-    message = "기존 포켓몬 #{} 수정 Form".format(pk)
-    return HttpResponse(message)
+    # 장고에서는 예상치못한 Exception은 500에러로 처리
+    # pokemon = Pokemon.objects.get(pk=pk)    # View에서는 권장하지 않음. Pokemon.DoesNotExist 에러 발생
+    pokemon = get_object_or_404(Pokemon, pk=pk)
+
+    if request.method == 'GET':
+        form = PokemonForm(instance=pokemon)
+    else:
+        # 뷰에서 참조할 수 있는 데이터 목록
+        # request.GET, request.POST, request.FILES
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon) # update : instance가 있기 때문
+        if form.is_valid():
+            form.save()  # ModelForm에서만 지원
+            return redirect('/pokemon/')
+        else:
+            form.errors
+
+    return render(request, 'pokemon/pokemon_form.html', {
+        'form': form,
+    })
 
