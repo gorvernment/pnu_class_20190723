40fe4de8d5fe98f15d8be5a901f63bc81583024f
gorvernment <48075099+gorvernment@users.noreply.github.com>
2019-07-24 13:28:55 +0900

    pokeform_new 구현

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from pokemon.forms import PokemonForm
 from pokemon.models import Pokemon
@@ -19,12 +19,23 @@ def index(request):
 #         'pokemon_list': qs,
 #     })
 
+
 def pokemon_new(request):
-    #return HttpResponse("새 포켓몬 등록 Form")
-    form = PokemonForm()
-    tag = form.as_table()
-    tag = '<table>' + tag + '</table>'
-    return HttpResponse(tag)
+    if request.method == 'GET':
+        form = PokemonForm()
+    else:
+        # 뷰에서 참조할 수 있는 데이터 목록
+        # request.GET, request.POST, request.FILES
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()  # ModelForm에서만 지원
+            return redirect('/pokemon/')
+        else:
+            form.errors
+
+    return render(request, 'pokemon/pokemon_form.html', {
+        'form': form,
+    })
 
 def pokemon_edit(request, pk):
     message = "기존 포켓몬 #{} 수정 Form".format(pk)
