45882e5e7a3fac329aa801775c6f0e97144ae8c3
gorvernment <48075099+gorvernment@users.noreply.github.com>
2019-07-24 11:31:21 +0900

    PokemonForm 빈 폼 구현

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from pokemon.forms import PokemonForm
 from pokemon.models import Pokemon
 
 
@@ -19,7 +20,11 @@ def index(request):
 #     })
 
 def pokemon_new(request):
-    return HttpResponse("새 포켓몬 등록 Form")
+    #return HttpResponse("새 포켓몬 등록 Form")
+    form = PokemonForm()
+    tag = form.as_table()
+    tag = '<table>' + tag + '</table>'
+    return HttpResponse(tag)
 
 def pokemon_edit(request, pk):
     message = "기존 포켓몬 #{} 수정 Form".format(pk)
